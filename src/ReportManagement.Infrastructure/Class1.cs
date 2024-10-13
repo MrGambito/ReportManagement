c11f6874cd94266e34db20bdd93fec5666075123
Manuel <--replace-all>
2024-10-13 12:27:04 -0500

    Base structure created.

---
+++ src/ReportManagement.Infrastructure/Class1.cs
@@ -0,0 +1,7 @@
+﻿namespace ReportManagement.Infrastructure
+{
+    public class Class1
+    {
+
+    }
+}
---
+++ src/ReportManager.Domain/Class1.cs
@@ -0,0 +1,7 @@
+﻿namespace ReportManagement.Domain
+{
+    public class Class1
+    {
+
+    }
+}
